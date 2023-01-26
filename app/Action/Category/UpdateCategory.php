5e841b515c09e5579f795e940a5f14535c2c1639
kudinov96 <ilya.kudinov96@yandex.ru>
2023-01-26 15:07:26 +0300

    add update index and parent_id for categories

---
+++ app/Action/Category/UpdateCategory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Action\Category;
+
+use App\Http\Requests\Category\CategoryUpdateRequest;
+use App\Models\Category;
+use Illuminate\Support\Facades\DB;
+
+class UpdateCategory
+{
+    public function handle(Category $item, CategoryUpdateRequest $request): Category
+    {
+        $item->index     = $request->index ?? $item->index;
+        $item->parent_id = $request->parent_id ?? $item->parent_id;
+
+        if ($request->index) {
+            DB::table("categories")
+                ->where("index", ">=", $item->index)
+                ->increment("index");
+        }
+
+        $item->save();
+
+        return $item;
+    }
+}
